a8894b68436c928208d7e7980c4855097e4ef352
glowysourworm <aniv.sm.esports@gmail.com>
2025-06-23 20:25:30 -0500

    init (finished)

---
+++ U2UConsult.HtmlTextBlock.Sample/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace U2UConsult.HtmlTextBlock.Sample
+{
+    using System.Windows;
+    using Microsoft.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StatusInformation si = new StatusInformation();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Formatting
+            this.HtmlFormattingRichTextBox.TextFormatter = new HtmlFormatter();
+            this.HtmlFormattingRichTextBox.Text =
+                @"<u>Scissors</u> cuts <i>paper</i>, <i>paper</i> covers <span style='color:orange'>rock</span>, <span style='color:orange'>rock</span> crushes <b style='color:blue'>lizard</b>, " +
+                "<b style='color:blue'>lizard</b> poisons <b>Spock</b>, <b>Spock</b> smashes <u>scissors</u>, <u>scissors</u> decapitates <b style='color:blue'>lizard</b>, " +
+                "<b style='color:blue'>lizard</b> eats <i>paper</i>, <i>paper</i> disproves <b>Spock</b>, <b>Spock</b> vaporizes <span style='color:orange'>rock</span>, " +
+                "and —as it <span style='font-size:24'><i>always</i></span> has— <span style='color:orange'>rock</span> crushes <u>scissors</u>";
+
+            // Tables
+            this.HtmlTableRichTextBox.Text = @"<table>" +
+                                              "<tr><td><span style='color: blue'>&nbsp;top left</span></td><td>&nbsp;top right</td></tr>" +
+                                              "<tr><td><a href='http://www.u2uconsult.com'>&nbsp;middle left</a></td><td>&nbsp;middle right</td></tr>" +
+                                              "<tr><td><i>&nbsp;bottom left</i></td><td><span style='font-size:18'>&nbsp;bottom right</span></td></tr>" +
+                                              "</table>";
+
+            // Binding
+            this.BoundHtmlRichTextBox.DataContext = this.si;
+
+            // Roll your own
+            // this.DiyRichTextBox.DataContext = this.YourHtml; // converter doesn'l like this
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.si.Culture = "nl";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.si.Culture = "fr";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.si.Culture = "en";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.DiyRichTextBox.Text = this.YourHtml.Text;
+        }
+    }
+}
---
+++ U2UConsult.HtmlTextBlock.Sample/StatusInformation.cs
@@ -0,0 +1,70 @@
+﻿namespace U2UConsult.HtmlTextBlock.Sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Just a sample class to illustrate the bindings ...
+    /// </summary>
+    public class StatusInformation : INotifyPropertyChanged
+    {
+        private string message = "no message yet";
+
+        private string culture;
+
+        public string Culture
+        {
+            get { return this.culture; }
+            set
+            {
+                this.culture = value;
+                switch (this.culture)
+                {
+                    case "nl":
+                        this.Message = "Het rapport werd naar <b>uw lokale printer</b> gestuurd.<br/><br/>Druk <u style='color:red'>Enter</u> om verder te gaan of <u style='color: red'>Keerekeweere</u> om opnieuw af te drukken.";
+                        break;
+                    case "fr":
+                        this.Message = "Le rapport a été envoyé a <b>votre imprimante locale</b>.<br/><br/>Appuyez sur <u style='color:red'>Entrée</u> pour continuer, ou <u style='color: red'>Retour</u> pour réimprimer.";
+                        break;
+                    default:
+                        this.Message = "The report was sent to <b>your local printer</b>.<br/><br/>Press <u style='color:red'>Enter</u> to continue or <u style='color:red'>Back</u> to print again.";
+                        break;
+                }
+            }
+        }
+
+
+        public string Message
+        {
+            get
+            {
+                return message;
+            }
+            set
+            {
+                message = value;
+                this.OnPropertyChanged("Message");
+            }
+        }
+
+        /// <summary>
+        /// Change notification.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// PropertyChanged Event Raiser.
+        /// </summary>
+        /// <param name="propertyName">Name of the modified property.</param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
